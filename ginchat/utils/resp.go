c2664c112dad45ab57c98205e45cdfcbcc386d21
binginx <sdaulibin@126.com>
2023-02-03 17:21:03 +0800

    golang study >> user resp

--- ginchat/utils/resp.go
+++ ginchat/utils/resp.go
@@ -1,6 +1,10 @@
 package utils
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type H struct {
 	Code  int
@@ -11,7 +15,18 @@ type H struct {
 }
 
 func Resp(w http.ResponseWriter, code int, data interface{}, msg string) {
-
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	h := H{
+		Code: code,
+		Data: data,
+		Msg:  msg,
+	}
+	ret, err := json.Marshal(h)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(ret)
 }
 
 func RespFail(w http.ResponseWriter, msg string) {
@@ -19,5 +34,24 @@ func RespFail(w http.ResponseWriter, msg string) {
 }
 
 func RespOk(w http.ResponseWriter, data interface{}, msg string) {
-	Resp(w, -1, data, msg)
+	Resp(w, 0, data, msg)
+}
+
+func RespOkList(w http.ResponseWriter, data interface{}, msg string, total int) {
+	RespList(w, 0, data, total)
+}
+
+func RespList(w http.ResponseWriter, code int, data interface{}, total int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	h := H{
+		Code:  code,
+		Rows:  data,
+		Total: total,
+	}
+	ret, err := json.Marshal(h)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(ret)
 }
